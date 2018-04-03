4916d0bf80739a3d8ec48996cd00ae3b272ee59f
Panos-JC <phatziioannou@gmail.com>
2018-04-03 22:10:49 +0300

    Initial client / server setup

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-app>
+      <page-header />
+      <main>
+        <v-content>
+          <v-container fluid>
+            <router-view></router-view>
+          </v-container>
+        </v-content>
+      </main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import PageHeader from '@/components/Header'
+export default {
+  name: 'App',
+  components: {
+    PageHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const config = require('./config/config')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+require('./routes')(app)
+
+app.listen(config.port, () => {
+  console.log(`Magic happens on port ${config.port}`)
+})
