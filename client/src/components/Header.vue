6c1a1f49a2f45afb03bbc85d3df6dc0e71c0455e
Panos-JC <phatziioannou@gmail.com>
2018-07-16 21:52:02 +0300

    add header menu

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -7,17 +7,52 @@
       <v-btn flat to="/browse">Browse</v-btn>
     </v-toolbar-items>
     <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat to="/login" v-if="!$store.state.isUserLoggedIn">
+    <v-toolbar-items v-if="!$store.state.isUserLoggedIn">
+      <v-btn flat to="/login">
         Login
       </v-btn>
-      <v-btn flat to="/register" v-if="!$store.state.isUserLoggedIn">
+      <v-btn flat to="/register">
         Register
       </v-btn>
-      <v-btn flat @click="logout" v-if="$store.state.isUserLoggedIn">
-        Log Out
-      </v-btn>
     </v-toolbar-items>
+    <v-btn icon v-if="$store.state.isUserLoggedIn">
+      <v-icon>notifications</v-icon>
+    </v-btn>
+
+    <v-menu left offset-y transition="slide-y-transition" v-if="$store.state.isUserLoggedIn">
+      <v-btn icon slot="activator" v-if="$store.state.isUserLoggedIn" class="mr-4">
+        <v-avatar size="32">
+          <img :src="'https://api.adorable.io/avatars/285/' + $store.state.user.properties.username + '.png'" alt="avatar">
+        </v-avatar>
+      </v-btn>
+      <v-list class="indigo" dark>
+        <v-list-tile  @click="profile">
+          <v-list-tile-action>
+            <v-icon>person</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content class="white--text">
+            <v-list-tile-title>Profile</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile  @click="profile">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content class="white--text">
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider></v-divider>
+        <v-list-tile @click="logout">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content class="white--text">
+            <v-list-tile-title>Log Out</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
   </v-toolbar>
 </template>
 
@@ -30,6 +65,12 @@ export default {
       this.$store.dispatch('setFollowing', null)
       this.$router.push({
         name: 'home'
+      })
+    },
+    profile () {
+      this.$router.push({
+        name: 'profile',
+        params: {username: this.$store.state.user.properties.username}
       })
     }
   }
