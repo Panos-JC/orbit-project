a295ea68d31155c3b2aaa13bf2c87a70d35ee6b0
Panos-JC <phatziioannou@gmail.com>
2019-07-18 11:59:29 +0300

    fix image error

--- client/src/components/homepage/ProfileCard.vue
+++ client/src/components/homepage/ProfileCard.vue
@@ -1,11 +1,10 @@
 <template>
 <v-card v-if="$store.state.isUserLoggedIn">
-  <v-img
-    class="white--text"
-    height="100px"
-    src="https://dummyimage.com/600x200/858585/ffffff"
+  <img
+    height="100%"
+    width="100%"
+    src="https://dummyimage.com/600x200/858585/a68fa6.jpg&text=+"
   >
-  </v-img>
   <v-card-title class="card-title">
     <div class="card-container">
       <v-avatar class="avatar" size="65">
