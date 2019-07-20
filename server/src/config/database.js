4f06507da11c6689f6f763d3c4eddd5e68d6619b
Panos-JC <phatziioannou@gmail.com>
2019-07-20 21:04:49 +0300

    edit database connection code

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,7 +1,7 @@
 const neo4j = require('neo4j')
 const config = require('./config.js')
 
-var db = new neo4j.GraphDatabase(config.neo4jUrl || 'http://neo4j:123@localhost:7474')
+var db = new neo4j.GraphDatabase(config.neo4jUrl)
 
 module.exports = {
   db
